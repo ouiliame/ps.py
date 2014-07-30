02c48792ce72535e267b4f18bf439704bf8f3ae2
William Chen <wchen298@gmail.com>
2014-07-30 10:36:30 -0700

    More friendly to install

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name='ps.py',
+	version='0.0.1',
+	packages=['ps', 'mechanize', 'xlwt'],
+	license='MIT',
+	url='http://ouiliame.github.io/ps.py/',
+	author='ouiliame',
+	author_email='wchen298@gmail.com'
+)
